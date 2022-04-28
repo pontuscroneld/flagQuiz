51e35db72167268b4065371db1a05251a9255365
Pontus Croneld <pontus.croneld@gmail.com>
2022-04-28 09:16:19 +0200

    Initial Commit

---
+++ flagQuiz/flagQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  flagQuizApp.swift
+//  flagQuiz
+//
+//  Created by Pontus Croneld on 2022-04-28.
+//
+
+import SwiftUI
+
+@main
+struct flagQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
